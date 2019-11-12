f7342944e48bf46783dc19eebea33c051326a4e1
Kern Rocke <kernrocke@hotmail.com>
2019-11-12 09:58:28 -0400

    Update to python algorithm script

--- BSS_SES/SES_BSS_ED.py
+++ BSS_SES/SES_BSS_ED.py
@@ -10,8 +10,25 @@
 #Import system modules
 import arcpy
 
-# Set environment settings
+# Set environment settings - Seetting current workspace (geodatabase with feature layers)
 arcpy.env.workspace = 'C:\Users\810000689\Documents\ArcGIS\Default.gdb'
+
+# Overwrite outputs of geoprocessing operations
+arcpy.env.overwriteOutput = True
+
+# Adding results of geoprocessing operations to display
+arcpy.env.addOutputsToMap = True
+
+# Activating Spatial Analyst Extension
+arcpy.CheckOutExtension("Spatial")
+
+# Activating Geostatistical Analyst Extension
+arcpy.CheckOutExtension("Geostats")
+
+# Activating Network Analyst Extension
+arcpy.CheckOutExtension("Network")
+
+# --------------------------------------------------------------------------------
 
 # Set local variables
 out_folder_path = "C:\Users\810000689\The University of the West Indies\DataGroup - repo_data\data_p145\version01\1-input\GIS Data"
@@ -29,3 +46,6 @@ arcpy.CreateFileGDB_management(out_folder_path, out_name)
 # Importing Features to File Geodatabase
 arcpy.FeatureClassToGeodatabase_conversion(inFeatures, outlocation)
 
+#Creating Layer for Health of the Nation sampled EDs
+HoTN_query = "[EDNum] = '1', & '2'"
+arcpy.MakeFeatureLayer_management("ED_SES", "HoTN_ED", HoTN_query)
